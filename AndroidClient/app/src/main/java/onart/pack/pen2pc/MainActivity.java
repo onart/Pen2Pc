8e4a86ecf8911bf21c7f311913e9b1911d040764
onart <onart97@naver.com>
2021-07-23 11:02:26 +0900

    android

---
+++ AndroidClient/app/src/main/java/onart/pack/pen2pc/MainActivity.java
@@ -0,0 +1,14 @@
+package onart.pack.pen2pc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
