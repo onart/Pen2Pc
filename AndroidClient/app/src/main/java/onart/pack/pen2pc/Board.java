45c16568f8eae5b623b9d89d1f62c9e54e145e5d
onart <onart97@naver.com>
2021-07-28 11:50:13 +0900

    그림판 프래그먼트

--- AndroidClient/app/src/main/java/onart/pack/pen2pc/Board.java
+++ AndroidClient/app/src/main/java/onart/pack/pen2pc/Board.java
@@ -2,17 +2,26 @@ package onart.pack.pen2pc;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.OrientationEventListener;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Board extends AppCompatActivity {
+
+    private FragmentContainerView paintSheet;
+    private boolean isLand;
+    public static int xmaxp=0, ymaxp=0, xmaxl=0, ymaxl=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +39,27 @@ public class Board extends AppCompatActivity {
         });
         findViewById(R.id.eraser).setOnClickListener(v -> clearRecord());
         findViewById(R.id.eraser2).setOnClickListener(v -> Toast.makeText(getApplicationContext(),"예",Toast.LENGTH_SHORT).show());
+        paintSheet=findViewById(R.id.paintSheet);
+        paintSheet.setOnTouchListener((View.OnTouchListener) (v, event) -> {
+            float x=event.getX();
+            float y=event.getY();
+            switch (event.getActionMasked()){
+                case MotionEvent.ACTION_DOWN:
+                    v.performClick();
+                    Log.i("down",String.format("%f %f",x,y));
+                    break;
+                case MotionEvent.ACTION_UP:
+                    Log.i("up",String.format("%f %f",x,y));
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    Log.i("move",String.format("%f %f",x,y));
+                    break;
+            }
+            return true;
+        });
         //1. PC 연결
         //2. 스레드 시작
         //3. 스레드 종료
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        float x=event.getX();
-        float y=event.getY();
-        if(y>1900f) return super.onTouchEvent(event);
-        else x+=1;
-        return super.onTouchEvent(event);
     }
 
     void clearRecord(){
@@ -51,6 +69,55 @@ public class Board extends AppCompatActivity {
     @Override
     public void onConfigurationChanged(@NonNull Configuration newConfig) {
         findViewById(R.id.eraser2).callOnClick();
+        isLand=newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE;
         super.onConfigurationChanged(newConfig);
+    }
+
+    void getMax(){
+        if(xmaxl>0)return;
+        if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE){
+            xmaxl=paintSheet.getWidth();
+            ymaxl=paintSheet.getHeight();
+            xmaxp=ymaxl;
+            ymaxp=xmaxl;
+            isLand=true;
+        }
+        else{
+            xmaxp=paintSheet.getWidth();
+            ymaxp=paintSheet.getHeight();
+            xmaxl=ymaxp;
+            ymaxl=xmaxp;
+            isLand=false;
+        }
+    }
+
+    void setRatio(int x, int y){
+        ViewGroup.LayoutParams lp=paintSheet.getLayoutParams();
+        getMax();
+        if(x==0){
+            lp.width=ViewGroup.LayoutParams.MATCH_PARENT;
+            lp.height=ViewGroup.LayoutParams.MATCH_PARENT;
+        }
+        else if(x>y){
+            double yOverX=(double)y/x;
+            lp.width=ViewGroup.LayoutParams.MATCH_PARENT;
+            if(isLand){
+                lp.height=(int) (yOverX*xmaxl);
+            }
+            else{
+                lp.height=(int) (yOverX*xmaxp);
+            }
+        }
+        else{
+            lp.height=ViewGroup.LayoutParams.MATCH_PARENT;
+            double xOverY=(double)x/y;
+            if(isLand){
+                lp.width=(int) (xOverY*ymaxl);
+            }
+            else{
+                lp.width=(int) (xOverY*ymaxp);
+            }
+        }
+        paintSheet.setLayoutParams(lp);
     }
 }
